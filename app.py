4db265a3cd57f0a81c4c0e6555edc4c87a9323d1
Masynchin <banzur.badurov@gmail.com>
2021-02-18 14:50:51 +0300

    add static files and basic template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route("/template")
+def template():
+    return render_template("index.html")
