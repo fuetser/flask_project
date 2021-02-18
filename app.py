90bf3494833802e13b1e0bcd61ae2a51427afe2e
Masynchin <banzur.badurov@gmail.com>
2021-02-18 22:51:45 +0300

    add posts to template

--- app.py
+++ app.py
@@ -10,4 +10,19 @@ def hello():
 
 @app.route("/template")
 def template():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        posts=[
+        {
+            "id": 123,
+            "author": {"nickname": "cool_user", "id": 1234},
+            "title": "Dogs are so cute!",
+            "elapsed": "2 hours ago",
+        },
+        {
+            "id": 456,
+            "author": {"nickname": "bad_user", "id": 5678},
+            "title": "Dogs aren't so cute!",
+            "elapsed": "1 hour ago",
+        }
+        ])
