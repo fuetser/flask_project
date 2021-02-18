c84f7bfceecc41fc0d764dbc60efd74e80de660f
Masynchin <banzur.badurov@gmail.com>
2021-02-19 00:15:28 +0300

    clickable posts, comments count, button color

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+LOREM_IPSUM = (
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+    "Cras venenatis vehicula libero et elementum. Sed imperdiet velit "
+    "tristique tempor sagittis. Donec sodales diam quam, in tempor dolor "
+    "faucibus tempus. Nulla dui erat, ultricies non facilisis non, eleifend "
+    "eu felis. Proin vel turpis ante. Suspendisse in odio nec nisi pulvinar "
+    "aliquam.".split())
+
+
 @app.route("/")
 def hello():
     return "Hello, World!"
@@ -17,16 +26,18 @@ def template():
             "author": {"nickname": "cool_user", "id": 1234},
             "title": "Dogs are so cute!",
             "elapsed": "2 hours ago",
-            "content": "a" * 200,
-            "likes": 100
+            "content": " ".join(LOREM_IPSUM[:25]),
+            "likes": 100,
+            "comments": 12,
         },
         {
             "id": 456,
             "author": {"nickname": "bad_user", "id": 5678},
             "title": "Dogs aren't so cute!",
             "elapsed": "1 hour ago",
-            "content": "b" * 200,
-            "likes": 1000
+            "content": " ".join(LOREM_IPSUM[:20]),
+            "likes": 1000,
+            "comments": 12,
         }
     ])
 
