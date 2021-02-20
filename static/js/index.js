16b54d749156dff9c506330792a0e5a470dcaa18
Alexander Zakharchuk <mystang.pro@gmail.com>
2021-02-20 16:13:12 +0300

    JS fixes

--- static/js/index.js
+++ static/js/index.js
@@ -2,23 +2,19 @@ const navbar = document.querySelector(".navbar")
 const body = document.querySelector("body")
 
 body.style.cssText = `padding-top: ${navbar.clientHeight}px;`
-var lastScroll = 0
+var lastScroll = window.pageYOffset * 2
 
-window.addEventListener("scroll", (event) => {
-    if (window.pageYOffset < lastScroll) {
-        if (navbar.classList.contains("scrolled-down")) {
-            navbar.classList.remove("scrolled-down")
-        }
+document.addEventListener("scroll", (event) => {
+    if (window.pageYOffset <= lastScroll) {
+        navbar.classList.remove("scrolled-down")
         navbar.classList.add("scrolled-up")
     } else {
-        if (navbar.classList.contains("scrolled-up")) {
-            navbar.classList.remove("scrolled-up")
-        }
+        navbar.classList.remove("scrolled-up")
         navbar.classList.add("scrolled-down")
     }
     lastScroll = window.pageYOffset
 })
 
-window.addEventListener("optimizedResize", (event) => {
+document.addEventListener("optimizedResize", (event) => {
     body.style.cssText = `padding-top: ${navbar.clientHeight}px;`
 })
