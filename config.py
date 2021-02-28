9bbf569cd263b0b5c5db6528dff68d431ad556ca
Masynchin <banzur.badurov@gmail.com>
2021-02-28 15:56:13 +0300

    remove unused db code

--- config.py
+++ config.py
@@ -9,5 +9,4 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or secrets.token_urlsafe(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         "sqlite:///" + os.path.join(basedir, "db.db")
-    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
