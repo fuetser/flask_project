2b98bb243a7d91152d859096ee3cc0d135186d41
Masynchin <banzur.badurov@gmail.com>
2021-04-10 11:44:54 +0300

    add  method to RawImage

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -5,6 +5,12 @@ class RawImage(NamedTuple):
     image_bytes: bytes
     mimetype: str
 
+    @classmethod
+    def from_wtf_file(cls, wtf_file):
+        image_bytes = convert_wtf_file_to_bytes(wtf_file)
+        mimetype = get_mimetype_from_wtf_file(wtf_file)
+        return cls(image_bytes, mimetype)
+
     def raise_for_image_validity(self):
         raise NotImplementedError
 
