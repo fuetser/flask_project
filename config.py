dc014b0064389a654dce50895561fef378b8be22
Masynchin <masynchin@gmail.com>
2021-04-18 19:30:51 +0300

    preparations for heroku

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or secrets.token_urlsafe(16)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "sqlite:///" + os.path.join(basedir, "db.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+        "postgres", "postgresql") or "sqlite:///" + os.path.join(basedir, "db.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_ALGORITHM = "HS256"
